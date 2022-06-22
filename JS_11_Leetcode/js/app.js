c1f2003a0e34aebd34b55c1cdf4c5a8b95062a4e
MartaSolo <marciamarcia@o2.pl>
2022-06-22 13:51:25 +0200

    added Leetcode - two sum

--- JS_11_Leetcode/js/app.js
+++ JS_11_Leetcode/js/app.js
@@ -106,27 +106,52 @@
 // rotate 3 steps to the right: [5,6,7,1,2,3,4]
 
 // v1 - mutating the original array
-const rotateArray1 = (array, k) => {
-  for (let i = 1; i <= k; i++) {
-    let el = array.pop();
-    array.unshift(el);
-  }
-  return array;
-};
+// const rotateArray1 = (array, k) => {
+//   for (let i = 1; i <= k; i++) {
+//     let el = array.pop();
+//     array.unshift(el);
+//   }
+//   return array;
+// };
 
 // console.log(rotateArray1([1, 2, 3, 4, 5, 6, 7], 3)); // [5, 6, 7, 1, 2, 3, 4]
 // leetcode - 1 error when testing
 
 // v2 - not mutating the original array
 
-const rotateArray2 = (array, k) => {
-  // const rotatedPart = array.slice(k + 1, array.length);
-  const rotatedPart = array.slice(array.length - k, array.length);
-  console.log("rotatedPart", rotatedPart);
-  const stillPart = array.slice(0, array.length - k);
-  console.log("stillPart", stillPart);
-  return [...rotatedPart, ...stillPart];
+// const rotateArray2 = (array, k) => {
+//   const rotatedPart = array.slice(array.length - k, array.length);
+//   const stillPart = array.slice(0, array.length - k);
+//   return [...rotatedPart, ...stillPart];
+// };
+
+// console.log(rotateArray2([1, 2, 3, 4, 5, 6, 7], 3)); // [5, 6, 7, 1, 2, 3, 4]
+// leetcode - not accepting a new array, just modifying the input array
+
+// -------------------------------------------------------
+
+// Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+const twoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        return [i, j];
+      }
+    }
+  }
 };
 
-console.log(rotateArray2([1, 2, 3, 4, 5, 6, 7], 3)); // [5, 6, 7, 1, 2, 3, 4]
-// leetcode - not accepting a new array, just modifying the input array
+// console.log(twoSum([2, 7, 11, 15], 9)); // [0,1]
+// console.log(twoSum([2, 7, 11, 15], 18)); // [1,2]
+console.log(twoSum([3, 3], 6)); // [0,1]
+console.log(twoSum([3, 2, 3], 6)); // [0, 2]
+console.log(twoSum([2, 7, 11, 15], 17)); // [0,3]
+console.log(twoSum([3, 2, 4, 5, 7, 8], 12)); // [2, 5]
