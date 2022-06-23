a55459c03d1c1d13ec23fb76dd10a14e17b4ca12
MartaSolo <marciamarcia@o2.pl>
2022-06-23 23:19:14 +0200

    added leetcode - plus one

--- JS_11_Leetcode/js/app.js
+++ JS_11_Leetcode/js/app.js
@@ -169,16 +169,41 @@
 // Input: nums = [0]
 // Output: [0]
 
-const moveZeros = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === 0) {
-      let zeroIndex = array.indexOf(array[i]);
-      array.push(array[i]);
-      console.log(array);
-      array.splice(zeroIndex, 1);
-    }
+// const moveZeros = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === 0) {
+//       let zeroIndex = array.indexOf(array[i]);
+//       array.push(array[i]);
+//       console.log(array);
+//       array.splice(zeroIndex, 1);
+//     }
+//   }
+//   return array;
+// };
+
+// console.log(moveZeros([0, 1, 0, 3, 12])); //  [1, 3, 12, 0, 0]
+
+// -------------------------------------------------------
+
+// Plus One
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+// Increment the large integer by one and return the resulting array of digits.
+
+// Example 1:
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+var plusOne = function (digits) {
+  for (var i = digits.length - 1; i >= 0; i--) {
+    if (++digits[i] > 9) digits[i] = 0;
+    else return digits;
   }
-  return array;
+  digits.unshift(1);
+  return digits;
 };
 
-console.log(moveZeros([0, 1, 0, 3, 12])); //  [1, 3, 12, 0, 0]
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([9, 9, 9]));
