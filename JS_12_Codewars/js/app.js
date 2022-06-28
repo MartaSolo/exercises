3f39e7159a24d97bc5226b82bff898532b2574c1
MartaSolo <marciamarcia@o2.pl>
2022-06-28 20:25:46 +0200

    Added Codewars: formatDuration

--- JS_12_Codewars/js/app.js
+++ JS_12_Codewars/js/app.js
@@ -509,3 +509,82 @@
 // console.log(domainName("www.xakep.ru")); // xakep
 
 // -------------------------- codewars 4kyu -------------------------------
+
+// Human readable duration format
+
+// Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+// The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+// It is much easier to understand with an example:
+
+// * For seconds = 62, your function should return
+//     "1 minute and 2 seconds"
+// * For seconds = 3662, your function should return
+//     "1 hour, 1 minute and 2 seconds"
+// For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+// Note that spaces are important.
+
+// Detailed rules
+// The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
+
+// The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+
+// A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+
+// Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+
+// A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+
+// A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+
+const formatDuration = (seconds) => {
+  switch (true) {
+    case seconds < 0:
+      return false;
+    case seconds === 0:
+      return "now";
+    case seconds < 60:
+      return seconds === 1 ? `1 second` : `${seconds}seconds`;
+  }
+
+  const duration = {};
+
+  const countDuration = (secondsNumber) => {
+    duration.year = parseInt(secondsNumber / (365 * 24 * 60 * 60));
+    duration.day = parseInt((secondsNumber / (24 * 60 * 60)) % 365);
+    duration.hour = parseInt((secondsNumber / (60 * 60)) % 24);
+    duration.minute = parseInt(secondsNumber / 60) % 60;
+    duration.second = parseInt(secondsNumber % 60);
+    return duration;
+  };
+  countDuration(seconds);
+  //   console.log(duration);
+
+  const dateArr = Object.entries(duration).filter((el) => {
+    return el[1] !== 0;
+  });
+
+  const dateFormatted = dateArr.map((el) => {
+    return el[1] === 1 ? `${el[1]} ${el[0]}` : `${el[1]} ${el[0]}s`;
+  });
+
+  switch (dateFormatted.length) {
+    case 1:
+      return `${dateFormatted[0]}`;
+    case 2:
+      return `${dateFormatted[0]} and ${dateFormatted[1]}`;
+    case 3:
+      return `${dateFormatted[0]}, ${dateFormatted[1]} and ${dateFormatted[2]}`;
+    case 4:
+      return `${dateFormatted[0]}, ${dateFormatted[1]}, ${dateFormatted[2]} and ${dateFormatted[3]}`;
+    case 5:
+      return `${dateFormatted[0]}, ${dateFormatted[1]}, ${dateFormatted[2]}, ${dateFormatted[3]} and ${dateFormatted[4]}`;
+    default:
+      "";
+  }
+};
+
+console.log(formatDuration(212345678));
+console.log(formatDuration(1));
+console.log(formatDuration(3662));
