21791b06828fec581d294f5014886662abdefccf
MartaSolo <marciamarcia@o2.pl>
2022-07-13 16:03:56 +0200

    added JS_15_FreeCodeCamp - Functional programkinf 40%

--- JS_15_FreeCodeCamp/js/app.js
+++ JS_15_FreeCodeCamp/js/app.js
@@ -104,18 +104,18 @@
 
 // We have defined a function named rangeOfNumbers with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
 
-function rangeOfNumbers(startNum, endNum) {
-  if (startNum === endNum) {
-    return [startNum];
-  } else {
-    const arr = rangeOfNumbers(startNum + 1, endNum);
-    arr.unshift(startNum);
-    return arr;
-  }
-}
+// function rangeOfNumbers(startNum, endNum) {
+//   if (startNum === endNum) {
+//     return [startNum];
+//   } else {
+//     const arr = rangeOfNumbers(startNum + 1, endNum);
+//     arr.unshift(startNum);
+//     return arr;
+//   }
+// }
 
-console.log(rangeOfNumbers(1, 5));
-console.log(rangeOfNumbers(4, 4));
+// console.log(rangeOfNumbers(1, 5));
+// console.log(rangeOfNumbers(4, 4));
 
 // ----------------------------------------------
 
@@ -176,4 +176,233 @@ console.log(rangeOfNumbers(4, 4));
 // console.log(lookUpProfile("Akira", "address")); // No such property
 // console.log(lookUpProfile("Kristian", "lastName")); // Vos
 
-// -----------------------------------------
+// -----------------------------------------------
+// ------ Functional programming -----------------
+
+// -----------------------------------------------
+
+// Refactor Global Variables Out of Functions
+
+// const bookList = [
+//   "The Hound of the Baskervilles",
+//   "On The Electrodynamics of Moving Bodies",
+//   "Philosophiæ Naturalis Principia Mathematica",
+//   "Disquisitiones Arithmeticae",
+// ];
+
+// console.log("bookList", bookList);
+
+// function add(bookArr, bookName) {
+//   const newBookArr = [...bookArr];
+//   newBookArr.push(bookName);
+//   return newBookArr;
+// }
+
+// function remove(bookArr, bookName) {
+//   const newBookArr = [...bookArr];
+//   const book_index = newBookArr.indexOf(bookName);
+//   if (book_index >= 0) {
+//     newBookArr.splice(book_index, 1);
+//     return newBookArr;
+//   }
+// }
+
+// console.log(add(bookList, "A Brief History of Time"));
+// console.log(remove(bookList, "On The Electrodynamics of Moving Bodies"));
+
+// -----------------------------------------------
+
+// Use the map Method to Extract Data from an Array
+
+// // The global variable
+// const watchList = [
+//   {
+//     Title: "The Dark Knight",
+//     Year: "2008",
+//     imdbRating: "9.0",
+//     imdbVotes: "1,652,832",
+//   },
+//   {
+//     Title: "Batman Begins",
+//     Year: "2005",
+//     imdbRating: "8.3",
+//     imdbVotes: "972,584",
+//   },
+//   {
+//     Title: "Avatar",
+//     Year: "2009",
+//     imdbRating: "7.9",
+//     imdbVotes: "876,575",
+//   },
+// ];
+
+// // const ratings = watchList.map((listItem) => {
+// //   return { Title: listItem.Title, imdbRating: listItem.imdbRating };
+// // });
+// const ratings = watchList.map((listItem) => {
+//   return { title: listItem.Title, rating: listItem.imdbRating };
+// });
+
+// console.log(JSON.stringify(ratings));
+
+// -----------------------------------------------
+
+// Implement map on a Prototype
+
+// const s = [23, 65, 98, 5];
+
+// Array.prototype.myMap = function (callback) {
+//   const newArray = [];
+
+//   for (let i = 0; i < this.length; i++) {
+//     let value = callback(this[i]);
+//     newArray.push(value);
+//   }
+//   return newArray;
+// };
+
+// const new_s = s.myMap(function (item) {
+//   console.log(item);
+//   return item * 2;
+// });
+// console.log(new_s); // [46, 130, 196, 10].
+
+// -----------------------------------------------
+
+// Use the filter Method to Extract Data from an Array
+
+// The variable watchList holds an array of objects with information on several movies. Use a combination of filter and map on watchList to assign a new array of objects with only title and rating keys. The new array should only include objects where imdbRating is greater than or equal to 8.0. Note that the rating values are saved as strings in the object and you may need to convert them into numbers to perform mathematical operations on them.
+
+// const watchList = [
+//   {
+//     Title: "Inception",
+//     Year: "2010",
+//     Rated: "PG-13",
+//     Released: "16 Jul 2010",
+//     Runtime: "148 min",
+//     Genre: "Action, Adventure, Crime",
+//     Director: "Christopher Nolan",
+//     Writer: "Christopher Nolan",
+//     Actors: "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+//     Plot: "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+//     Language: "English, Japanese, French",
+//     Country: "USA, UK",
+//     Awards: "Won 4 Oscars. Another 143 wins & 198 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//     Metascore: "74",
+//     imdbRating: "8.8",
+//     imdbVotes: "1,446,708",
+//     imdbID: "tt1375666",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "Interstellar",
+//     Year: "2014",
+//     Rated: "PG-13",
+//     Released: "07 Nov 2014",
+//     Runtime: "169 min",
+//     Genre: "Adventure, Drama, Sci-Fi",
+//     Director: "Christopher Nolan",
+//     Writer: "Jonathan Nolan, Christopher Nolan",
+//     Actors: "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+//     Plot: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+//     Language: "English",
+//     Country: "USA, UK",
+//     Awards: "Won 1 Oscar. Another 39 wins & 132 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+//     Metascore: "74",
+//     imdbRating: "8.6",
+//     imdbVotes: "910,366",
+//     imdbID: "tt0816692",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "The Dark Knight",
+//     Year: "2008",
+//     Rated: "PG-13",
+//     Released: "18 Jul 2008",
+//     Runtime: "152 min",
+//     Genre: "Action, Adventure, Crime",
+//     Director: "Christopher Nolan",
+//     Writer:
+//       "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+//     Actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//     Plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+//     Language: "English, Mandarin",
+//     Country: "USA, UK",
+//     Awards: "Won 2 Oscars. Another 146 wins & 142 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+//     Metascore: "82",
+//     imdbRating: "9.0",
+//     imdbVotes: "1,652,832",
+//     imdbID: "tt0468569",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "Batman Begins",
+//     Year: "2005",
+//     Rated: "PG-13",
+//     Released: "15 Jun 2005",
+//     Runtime: "140 min",
+//     Genre: "Action, Adventure",
+//     Director: "Christopher Nolan",
+//     Writer:
+//       "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+//     Actors: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+//     Plot: "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+//     Language: "English, Urdu, Mandarin",
+//     Country: "USA, UK",
+//     Awards: "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+//     Metascore: "70",
+//     imdbRating: "8.3",
+//     imdbVotes: "972,584",
+//     imdbID: "tt0372784",
+//     Type: "movie",
+//     Response: "True",
+//   },
+//   {
+//     Title: "Avatar",
+//     Year: "2009",
+//     Rated: "PG-13",
+//     Released: "18 Dec 2009",
+//     Runtime: "162 min",
+//     Genre: "Action, Adventure, Fantasy",
+//     Director: "James Cameron",
+//     Writer: "James Cameron",
+//     Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+//     Plot: "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+//     Language: "English, Spanish",
+//     Country: "USA, UK",
+//     Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+//     Poster:
+//       "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+//     Metascore: "83",
+//     imdbRating: "7.9",
+//     imdbVotes: "876,575",
+//     imdbID: "tt0499549",
+//     Type: "movie",
+//     Response: "True",
+//   },
+// ];
+
+// // Only change code below this line
+
+// const filteredList = watchList
+//   .map((listItem) => {
+//     return { title: listItem.Title, rating: listItem.imdbRating };
+//   })
+//   .filter((listItem) => Number(listItem.rating) >= 8.0);
+
+// // Only change code above this line
+
+// console.log(filteredList);
+
+// -----------------------------------------------
